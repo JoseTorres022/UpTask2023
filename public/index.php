4561e6ea9f874158e79cc647398834b59c0d0db8
Jose Torres <joseangel.tosa@outlook.com>
2023-04-07 15:29:13 -0600

    refactor code 1

--- public/index.php
+++ public/index.php
@@ -3,7 +3,29 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 $router = new Router();
+
+//Login
+$router->get('/',[LoginController::class,'login']);
+$router->post('/',[LoginController::class,'login']);
+$router->get('/logout',[LoginController::class,'logout']);
+
+//Crear cuenta
+$router->get('/crear',[LoginController::class,'crear']);
+$router->post('/crear',[LoginController::class,'crear']);
+
+//Formuadio de olvide mi password
+$router->get('/olvide',[LoginController::class,'olvide']);
+$router->post('/olvide',[LoginController::class,'olvide']);
+
+//Colocal el nuevo password
+$router->get('/restablecer',[LoginController::class,'restablecer']);
+$router->post('/restablecer',[LoginController::class,'restablecer']);
+
+//Confirmacion de cuenta
+$router->get('/mensaje',[LoginController::class,'mensaje']);
+$router->post('/confirmar',[LoginController::class,'confirmar']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
---
+++ views/auth/login.php
@@ -0,0 +1,27 @@
+<div class="contenedor crear">
+    <h1>UpTask</h1>
+    <p class="tagline">Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form class="formulario" method="POST" action="/">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" placeholder="Tu Email" name="email" />
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Tu Password" name="password" />
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta?. Obtener una.</a>
+            <a href="/olvide">¿Olvidaste tu Password?.</a>
+        </div>
+    </div>
+    <!--.contenedor-sm-->
+</div>
