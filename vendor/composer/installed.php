84afb1e42ad743dbd721c3511617f2d0d67daa37
Jose Torres <joseangel.tosa@outlook.com>
2023-04-07 14:55:07 -0600

    refactor code, running corrently

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'juandelatorre/up-task_mvc',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '535d44a780f2719dbcfbe87e5b34d1d99da11004',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'juandelatorre/up-task_mvc' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '535d44a780f2719dbcfbe87e5b34d1d99da11004',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
